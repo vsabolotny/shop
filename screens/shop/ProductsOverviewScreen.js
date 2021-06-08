4a2fe9eedc9dfe3c1fe3082f2f1a134aacbf7e27
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-06-08 08:49:07 +0200

    Add product model with dummy data

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+const ProductsOverviewScreen = (props) => {
+  return <FlatList></FlatList>;
+};
+
+export default ProductsOverviewScreen;
