3b6f13c4227149217bc5f444d7b1abc58064ec72
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-06-17 14:41:07 +0200

    Add error handling and loading spinner

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,29 +1,81 @@
-import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  FlatList,
+  Button,
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import HeaderButton from '../../components/UI/HeaderButton';
-import ProductItem from '../../components/shop/ProductItem';
-import * as cartActions from '../../store/actions/cart';
-import Colors from '../../constants/colors';
+import HeaderButton from "../../components/UI/HeaderButton";
+import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
+import * as productActions from "../../store/actions/products";
+import Colors from "../../constants/colors";
 
-const ProductsOverviewScreen = props => {
-  const products = useSelector(state => state.products.availableProducts);
+const ProductsOverviewScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const products = useSelector((state) => state.products.availableProducts);
   const dispatch = useDispatch();
 
+  // const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    // await delay(5000);
+    try {
+      dispatch(productActions.fetchProducts());
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    loadProducts();
+  }, [dispatch, loadProducts]);
+
   const selectItemHandler = (id, title) => {
-    props.navigation.navigate('ProductDetail', {
+    props.navigation.navigate("ProductDetail", {
       productId: id,
-      productTitle: title
+      productTitle: title,
     });
   };
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>An error occured</Text>
+        <Button title="Try again" onPress={loadProducts} />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No products found. Maybe start adding some!</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
         <ProductItem
           image={itemData.item.imageUrl}
           title={itemData.item.title}
@@ -79,5 +131,9 @@ export const screenOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  centered: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
 
 export default ProductsOverviewScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,44 @@
+import Product from "../../models/product";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://rn-shop-5b622-default-rtdb.firebaseio.com/products.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send analytics
+      throw err;
+    }
+  };
+};
 
 export const deleteProduct = (productId) => {
   return { type: DELETE_PRODUCT, pid: productId };
@@ -25,8 +63,6 @@ export const createProduct = (title, description, imageUrl, price) => {
     );
 
     const resData = await response.json();
-
-    console.log(resData);
 
     dispatch({
       type: CREATE_PRODUCT,
